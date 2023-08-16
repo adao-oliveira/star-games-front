5a0448e22273d5388ef7845db8276c2b47601a3b
Adao Domingos <adaooliveira44@gmail.com>
2023-08-16 19:39:29 -0300

    ajuste na url

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-export const server = "https://star-games-server.vercel.app/api/v2";
-export const backend_url = "https://star-games-server.vercel.app/";
+export const server = "https://star-games-server-1uu34zxmd-adao-oliveira.vercel.app/api/v2";
+export const backend_url = "https://star-games-server-1uu34zxmd-adao-oliveira.vercel.app/";
 
 
